0fb59a3d3d526020ee215acdfc9af1d290199b58
datuedw <edwardo_r_datu@manulife.com>
2022-07-16 08:21:13 +0800

    Add Treblle API management

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import com.treblle.spring.annotation.EnableTreblle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableTreblle
 @SpringBootApplication
 public class DemoApplication {
 
